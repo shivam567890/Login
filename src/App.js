bc9afa05615f555ce42e8020f2042f959dead7e8
shivam567890 <shivam.kh007@gmail.com>
2023-08-22 12:43:51 +0530

    loginpage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
-
+import XeroCode from './Img/Logo4.svg';
+import Img from  './Img/Vector.png';
+import Img66 from './Img/Img66.svg';
+import Google from './Img/google.svg';
+import GitHub from './Img/image2.svg';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+       
+    <div className="flex-container">
+      
+        <div className='left'>
+           <img src={XeroCode} alt="Logo" width="160px" className='logo'/>
+           <div className='card'>
+           <h1>Welcome Arya Soni !</h1>
+               <div className='line'> 
+                   <hr className='half_line'/>
+                   Login To Your Account
+                   <hr  className='half_line'/>
+                 </div>
+
+                 <div className='form'>
+                   <input type="text" className='input'/>
+                   <input type="text" className='input'/>
+                   <button className='input btn'>LOGIN</button>                 
+                
+                 <p className='OR'> OR</p>
+
+                 <div className='logins'>
+                       
+                        <div className='google'>
+                          <p>Login With Google</p>
+                           <img src={Google} alt="login" style={{marginLeft:"4px"}}/>
+                        </div>
+                        
+
+                        <div className='google'>
+                          <p>Login With GitHub</p>
+                           <img src={GitHub} alt="login" style={{marginLeft:"4px"}}/>
+                        </div>
+                        </div>
+                 </div>
+               </div>
+        </div>
+        <hr className='center_hr'/>
+        <div className='right'>
+            <img src={Img66} alt="llooggoo"  />
+            <img src={Img} alt="bluecorner"  className='down'/>
+        </div>
     </div>
+     
+    </>
   );
 }
 
